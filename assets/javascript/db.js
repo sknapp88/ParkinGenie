c5bcddb6f143492c8ca1b3fe60c77fcba967403c
Seussan <Seussan>
2017-04-05 21:29:29 -0400

    commit so we can move

---
+++ assets/javascript/db.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC7sur-winD7BqDB3TRLB8aO49-b_XBLWA",
+  authDomain: "parkingenie-3ab7b.firebaseapp.com",
+  databaseURL: "https://parkingenie-3ab7b.firebaseio.com",
+  projectId: "parkingenie-3ab7b",
+  storageBucket: "parkingenie-3ab7b.appspot.com",
+  messagingSenderId: "790386734940"
+};
+
+firebase.initializeApp(config);
+
+var dbRef = firebase.database();
+
